c624f5af8ec3011cc36848080c60ad84a4dd2830
unknown <a.abdelazizg@gmail.com>
2021-06-18 22:39:44 +0200

    add final js edit

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 // nav change color when scroll
-let captionOffset = $(".caption").offset().top;
 
 $(window).scroll(function(){
     
     // nav change color
     let theScrollTop = $(window).scrollTop();
 
-    if(theScrollTop > captionOffset - 100 ){
+    if(theScrollTop > 39 ){
         $(".navs").css({"backgroundColor":"#f5f5f5", "padding":".5rem"})
         $(".logo img").attr("src","./img/bakery-color.png")
         $(".up").fadeIn(500)
         $(".up").css("display","flex")
+        $(".navlinks").css("text-align","center")
+        $(".menu-btn div").css("background-color","#000")
     }
     else
     {
@@ -18,10 +19,11 @@ $(window).scroll(function(){
         $(".logo img").attr("src","./img/bakery-light-1.png")
         $(".up").fadeOut(500)
         $(".up").css("display","none")
+        $(".navlinks").css("text-align","center")
+        $(".menu-btn div").css("background-color","#f8f8f8")
     }
 
 });
-
 
 // blog ui
 
@@ -50,12 +52,52 @@ $(".ancur a").click(function(e){
 
 });
 
-// sitting-icon & cart-icon
+// Color box
 
-$(".sitting-icon , .cart-icon").click(function(){
-    console.log("A")
-})
+$(".boxIcon").click(function(){
+
+    let colorsWidth = $(".colors").innerWidth();
+
+    if( $(".colorBox").css("left") == "-192.562px")
+    {
+        $(".colorBox").animate({"left":0} , 1000);
+    }
+    else
+    {
+        $(".colorBox").animate({"left":`-${colorsWidth}`} , 1000);
+    }
+
+});
 
 
+var allColors = ["red","green","orange","blue","black","white"]
+let allSpans = $(".colors span");
 
 
+for(var i=0;i<allColors.length;i++){
+    allSpans.eq(i).css("background-color",`${allColors[i]}`)
+}    
+
+
+allSpans.click(function(e){
+    
+    let theColorSelected = $(e.target).css("background-color");
+    $(".colorChange").css("color",`${theColorSelected}`)    
+});
+
+// menu btn
+
+$(".menu-btn").click(function(){
+
+    if($(".navlinks").offset().top == 0)
+    {
+        $(".navlinks .ancur").css("border","none")
+        $(".navlinks").css({"width":"50%"})
+        $(".navlinks").slideDown(800)
+    }
+    else
+    {
+        $(".navlinks").slideUp(800)
+    }
+
+});
